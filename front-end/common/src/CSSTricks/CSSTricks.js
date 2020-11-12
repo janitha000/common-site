85feb6faed71a409196811fe9ceeddcaf12b36f8
Janitha Tennakoon <janitha000@gmail.com>
2020-11-12 11:27:41 +0800

    Added css tricks

---
+++ front-end/common/src/CSSTricks/CSSTricks.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './CSSTricks.css'
+
+const CSSTricks = () => {
+    return (
+        <>
+            <div style={{ "display": "flex", "justifyContent": "space-around" }}>
+                <div className="parent">
+                    <div className="child"></div>
+                </div>
+
+                <div className="parent2">
+                    <div className="child2"></div>
+                </div>
+            </div>
+
+
+            <div style={{ "margin-top": "40px" }}>
+                <input type="text"></input>
+                <div>Div item</div>
+                <div className="red">Div item with red</div>
+                <div>
+                    <span>
+                        <b>This is inside div and span</b>
+                    </span>
+                </div>
+                <span>Span Item</span>
+                <ul>
+                    <li >Item 1</li>
+                    <li className="green">Item 2</li>
+                    <li className="brown">Item 3</li>
+                    <li >Item 4</li>
+                    <li>Item 5</li>
+                </ul>
+                <p data-blue="123">Hello</p>
+                <ul className="elements">
+                    <li >Item 1</li>
+                    <li >Item 2</li>
+                    <li >Item 3</li>
+                    <li >Item 4</li>
+                    <li>Item 5</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default CSSTricks
